114bd477871f1981a9f48a7282d8aefcebe0b6e2
holubiev <holubiev@vergilio.cz>
2025-06-21 21:06:19 +0200

    feat: add title metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import NavigationHistoryProvider from '@/contexts/NavigationHistory'
 import '@/styles/globals.css'
+import { Metadata } from 'next'
 import { FC, ReactNode } from 'react'
+
+export const metadata: Metadata = { title: 'Todos' }
 
 interface RootLayoutProps {
     children: ReactNode
